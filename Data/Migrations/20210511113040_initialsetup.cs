48011b430c6e0ff62be588cbf2d6c0ab0eb8cde4
SadrulToaha <sadrul.islamt@gmail.com>
2021-05-12 12:56:56 +0600

    Add project files.

---
+++ Data/Migrations/20210511113040_initialsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsPortal.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NewsCap = table.Column<string>(nullable: true),
+                    NewsPost = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "News");
+        }
+    }
+}
---
+++ Models/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string NewsCap { get; set; }
+        public string NewsPost { get; set; }
+
+        public News()
+        {
+
+        }
+
+    }
+}
